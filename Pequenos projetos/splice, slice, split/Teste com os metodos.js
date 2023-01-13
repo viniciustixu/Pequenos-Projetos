e6824d27c158c0a63fabcbbf3e00bc682249b2c4
viniciustixu <vinicipkt@live.com>
2023-01-13 19:33:16 -0300

    update

--- Pequenos projetos/splice, slice, split/Teste com os metodos.js
+++ Pequenos projetos/splice, slice, split/Teste com os metodos.js
@@ -1,12 +1,18 @@
-let texto = document.getElementById('txta1').value
+let texto = document.getElementById('txta1')
 let res = document.getElementById('resultado')
 
 
 
-let arr = []
-    
 
+let arr = ['@Usuario bla ble bli blo blu', '@']
+    
+/* Analisa a String, acha o @ e o espaço
+Joga os caracteres entre eles pra dentro de um array
+subtrai o pedaço que foi pro array da string
+repete até o texto acabar ou não encontrar mais @ */
 
 function calcular() {
-    res.innerHTML = `${typeof(texto)}asd`
+    //pausei pra aprender expressão regular
+    res.innerHTML = `${b}a`
 }
+ 
