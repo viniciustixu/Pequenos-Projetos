300b956577e3d207e39abbfa7e00a935bdaff8d6
viniciustixu <vinicipkt@live.com>
2023-01-13 16:51:28 -0300

    update

---
+++ Pequenos projetos/splice, slice, split/Teste com os metodos.js
@@ -0,0 +1,12 @@
+let texto = document.getElementById('txta1').value
+let res = document.getElementById('resultado')
+
+
+
+let arr = []
+    
+
+
+function calcular() {
+    res.innerHTML = `${typeof(texto)}asd`
+}
