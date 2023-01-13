b25865f080cdd3265b24f167e25233d3f3a25831
viniciustixu <vinicipkt@live.com>
2023-01-13 11:29:41 -0300

    split

--- Pequenos projetos/splice, slice, split/slice.js
+++ Pequenos projetos/splice, slice, split/slice.js
@@ -5,6 +5,8 @@ let numbers = [0, 1, 2, 3, 4, 5]
 
 //(ponto inicial, ponto final)
 let strWord = word.slice(0,5)
+let arrNumbers = numbers.slice(0,3)
 
 
-console.log(`${word.slice(0,5)}`)
+console.log(`${strWord}`)
+console.log(`${arrNumbers}`)
---
+++ Pequenos projetos/splice, slice, split/split.js
@@ -0,0 +1,8 @@
+//split só pode ser usada para strings
+let word = 'hello, world, again'
+
+
+//(primeiro parametro é um separador como "" ou ",",)
+let wordConverted = word.split(',', 2)
+
+console.log(`${wordConverted}`)
