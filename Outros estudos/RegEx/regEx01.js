df0c29193234e07fcd9d576a73f33b95b919f210
viniciustixu <vinicipkt@live.com>
2023-01-14 17:19:29 -0300

    Realocando

--- Outros estudos/RegEx/regEx01.js
+++
@@ -1,4 +0,0 @@
-let texto = '@MosFazli	Mostafa Fazli MosFazli\nAn Ordinary Programmer\n Shahroud university of technology  Mashhad\n\n@LukeberryPi	Lukeberry Pi LukeberryPi\nBritish-brazilian Software Engineer at Hostgator @lucasInutil'
-let matches = texto.match(/^@\w+/gm);
-
-console.log(matches);
