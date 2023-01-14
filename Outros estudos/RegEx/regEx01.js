cefb6878c6eb2f8f09ccf25f6931a12350152703
viniciustixu <vinicipkt@live.com>
2023-01-14 11:00:41 -0300

    update

---
+++ Outros estudos/RegEx/regEx01.js
@@ -0,0 +1,5 @@
+let texto = '@zakk616	Muhammad Zakaria zakk616Full-Stack Software Developer | Dotnet, Flask, Django, Nodejs | Machine Learning BMC Solution  Karachi, Pakistan. @lucky7123	lucky7123 lucky7123	'
+let c = [texto.match(/@\w+\	/g)];
+
+
+console.log(c)
--- Pequenos projetos/RegEx/regEx01.js
+++
@@ -1,4 +0,0 @@
-let texto = 'abcd dcba 123 4321 @asasdasdasdasdasdabube @satanael  @tixu @capivara asdasdasdasdsadsadasdasdasdasdasdasdasdasdasdasdasdwqewqeqweqweasdasdasdasdd'
-let c = [texto.match(/@\w+\s/g)];
-
-console.log(c)
