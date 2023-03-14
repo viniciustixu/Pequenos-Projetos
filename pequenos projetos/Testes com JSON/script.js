5e78e2a26832de7b9896667a3ff90a5238fa9b92
Vinicius Souza <vinicipkt@live.com>
2023-03-14 03:05:54 -0300

    Testes com JSON

--- pequenos projetos/Testes com JSON/script.js
+++ pequenos projetos/Testes com JSON/script.js
@@ -1,5 +1,25 @@
-const fs = require('fs'); // Módulo do Node.js para leitura de arquivos
-const jsonString = fs.readFileSync('arquivo.json', 'utf-8'); // Lê o arquivo e armazena em uma variável
-const arquivo = JSON.parse(jsonString);
+var container = document.getElementById("container");
 
-console.log(arquivo.uva.preco);
+fetch('arquivo.json')
+  .then(response => response.json())
+  .then(data => {
+    data.itens.forEach(function(item) {
+        var div = document.createElement("div");
+        var nome = document.createElement("h3");
+        var preco = document.createElement("p");
+        var tipo = document.createElement("p");
+        var origem = document.createElement("p");
+      
+        nome.innerText = item.nome;
+        preco.innerText = item.preco;
+        tipo.innerText = item.tipo;
+        origem.innerText = item.origem;
+      
+        div.appendChild(nome);
+        div.appendChild(preco);
+        div.appendChild(tipo);
+        div.appendChild(origem);
+      
+        container.appendChild(div);
+      });
+  });
